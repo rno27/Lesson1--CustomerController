2f287f51fb8dadbb9af776a7258354c54d5dbac1
rno27 <ivanescurenato@yahoo.com>
2019-04-17 11:38:35 +0300

    Add files via upload

---
+++ Lesson1/MenuItem.cs
@@ -0,0 +1,16 @@
+using System;
+namespace Lesson1.UI
+{
+    public class MenuItem
+    {
+        public int ActivationNumber { get; set; }
+        public string TextMessage { get; set; }
+        public Menu SubMenu { get; set;}
+        public Action ActionToExecute { get; set; }
+
+        public void RenderItem()
+        {
+            Console.WriteLine($"{ActivationNumber}. {TextMessage}");
+        }
+    }
+}
---
+++ Lesson1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Lesson1.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataRepository repository = new DataRepository();
+            repository.Initialize();
+
+            ConsoleMenuController menuController = new ConsoleMenuController(repository);
+            menuController.Initialize();
+            menuController.EnterMainMenu();
+            
+        }
+    }
+}
